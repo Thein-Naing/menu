3df5a1edc686968be099b69060d3f6ce62e9ebdf
Thein Naing <theinnaing0908@gmail.com>
2023-05-11 15:59:32 +0800

    return menuItem object with article/img tags and classNames

--- src/Menu.js
+++ src/Menu.js
@@ -6,6 +6,12 @@ return (
     <div className='section-center'>
       {items.map((menuItem) => {
         const {id, title, img, desc} = menuItem;
+        return (
+          <article  key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo' />
+
+          </article>
+        )
 
     })}
 
