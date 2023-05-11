15ec97607348b608651fa0721984f99cf2372c77
Thein Naing <theinnaing0908@gmail.com>
2023-05-11 15:51:24 +0800

    rendering <Menu /> in App.js, we need to define Menu.js and create Menu function. in Menu.js return function, first create menuItem object by destruction keys and map the items with menuItem argument

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
-};
+// {items} meaning we will render new values from this argument.
+const Menu = ({items}) => {
+return (
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const {id, title, img, desc} = menuItem;
+
+    })}
+
+    </div>
+)};
 
 export default Menu;
